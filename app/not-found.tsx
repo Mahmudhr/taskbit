82ad4235e2eeddcb680a54a05328a5e805667cf5
Imran <mahmud.bubt.150@gmail.com>
2025-08-17 15:16:23 +0600

    add not found content

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,15 +1,13 @@
 import { Button } from '@/components/ui/button';
-import { Lock } from 'lucide-react';
 import Link from 'next/link';
 
 export default function NotFound() {
   return (
     <div className='flex justify-center h-screen items-center'>
       <div className='flex flex-col justify-center items-center gap-4'>
-        <div className='text-2xl font-bold'>Access Denied</div>
-        <Lock className='w-12 h-12' />
-        <div className='text-gray-600'>
-          You&apos;re not permission to access this page
+        <div className='text-4xl font-bold'>404</div>
+        <div className='text-gray-600 dark:text-gray-300'>
+          Oops, This Page Is Not Found
         </div>
         <div>
           <Link href='/dashboard/my-tasks'>
