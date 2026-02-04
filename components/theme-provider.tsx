01d8b1b9e5d599a4a4164c7c581e627c898de889
Imran <mahmud.bubt.150@gmail.com>
2026-02-05 03:30:18 +0600

    remove maintenance mode

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -4,7 +4,6 @@ import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import type { ThemeProviderProps } from 'next-themes';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SessionProvider } from 'next-auth/react';
-import MaintenancePage from './maintenance';
 
 const queryClient = new QueryClient();
 
@@ -13,11 +12,7 @@ export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
     <NextThemesProvider {...props}>
       <SessionProvider>
         <QueryClientProvider client={queryClient}>
-          {process.env.NEXT_PUBLIC_MAINTENANCE_NOT_MODE !== 'true' ? (
-            <MaintenancePage />
-          ) : (
-            children
-          )}
+          {children}
         </QueryClientProvider>
       </SessionProvider>
     </NextThemesProvider>
